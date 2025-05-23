3a14165badde16f72fae6453848efe4f8975464a
masajinobe-ef <priscilla.effects@gmail.com>
2025-05-23 22:06:32 +0500

    new

--- modules/shared/pkgs.nix
+++ modules/shared/pkgs.nix
@@ -2,7 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # Development Tools
     vim
     neovim
     git
@@ -28,8 +27,6 @@
     llvm
     lld
     libiconv
-
-    # Terminal Utilities
     alacritty
     zsh
     tmux
@@ -46,8 +43,6 @@
     ventoy-full
     woeusb
     ntfs3g
-
-    # Networking & Security
     wget
     qbittorrent
     dnsmasq
@@ -58,15 +53,11 @@
     nftables
     acpid
     iproute2
-
-    # System Utilities & Security
     fastfetch
     btop
     sops
     age
     lynis
-
-    # Multimedia & Graphics
     xfce.ristretto
     xfce.tumbler
     krita
@@ -83,8 +74,6 @@
     libva
     libva-utils
     libvdpau-va-gl
-
-    # Desktop Environment & Wayland Tools
     gnome-keyring
     gnome-session
     gnome-shell
@@ -102,17 +91,19 @@
     rofi-wayland
     mako
     swaybg
-
-    # Archive Utilities
     p7zip
     unrar
     zip
     unzip
-
-    # Audio
     pavucontrol
-
-    # Web Browser
     firefox
+  stylua
+  shfmt
+  nixfmt
+  ruff
+  ktlint
+  clang-tools
+  prettierd
+  codespell
   ];
 }
