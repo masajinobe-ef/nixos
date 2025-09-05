bae79ea4bf09cb5be53d67903d871e077721efde
masajinobe-ef <priscilla.effects@gmail.com>
2025-09-05 15:58:11 +0500

    Maintenance: 2025-09-05 15:58:11

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -4,6 +4,11 @@
 {
   virtualisation.docker = {
     enable = true;
+    daemon.settings = {
+      dns = [ "8.8.8.8" "8.8.4.4" ];
+      dns-opts = [ "timeout:2" "attempts:2" ];
+      dns-search = [ "1.1.1.1" "77.88.8.8" ];
+    };
     rootless = {
       enable = true;
       setSocketVariable = true;
